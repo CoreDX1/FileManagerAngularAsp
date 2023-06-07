fab0d2a0d8ad0aa8815ee345e65076b9bc6a5318
CoreDX1 <chrismaquis@gmail.com>
2023-06-07 16:17:55 -0300

    Changed the name of the parameters

--- src/File.Infrastructure/Persistences/Interfaces/IFolderRepository.cs
+++ src/File.Infrastructure/Persistences/Interfaces/IFolderRepository.cs
@@ -4,7 +4,7 @@ namespace File.Infrastructure.Persistences.Interfaces;
 
 public interface IFolderRepository
 {
-    public Task<bool> GetByName(string name);
+    public Task<Folder> GetByName(Folder folder);
     public Task<bool> Create(Folder folder);
     public Task<bool> Update(string folder);
 }
