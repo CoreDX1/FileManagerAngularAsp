dc11a9ea7eaa97990a167788aa0a93eda18e0078
CoreDX1 <chrismaquis@gmail.com>
2023-06-07 18:41:17 -0300

    The messages of the responses

---
+++ src/File.Utilities/Static/ReplyMessage.cs
@@ -0,0 +1,19 @@
+namespace File.Utilities.Static;
+
+public class ReplyMessage
+{
+    public const string MESSAGE_QUERY_SUCCESS = "Se encontraron registros";
+    public const string MESSAGE_QUERY_EMPTY = "No se encontraron registros";
+    public const string MESSAGE_SAVE_SUCCESS = "Archivo guardado correctamente";
+    public const string MESSAGE_SAVE_ERROR = "Error al guardar el archivo";
+    public const string MESSAGE_UPDATE_SUCCESS = "Archivo actualizado correctamente";
+    public const string MESSAGE_DELETE_SUCCESS = "Archivo eliminado correctamente";
+    public const string MESSAGE_EXISTS = "El archivo ya existe";
+    public const string MESSAGE_ACTIVATE_SUCCESS = "Archivo activado correctamente";
+    public const string MESSAGE_TOKEN_GENERATED = "Token generado correctamente";
+    public const string MESSAGE_TOKEN_ERROR =
+        "El usuario y/o contraseña es incorrecta, verifíquelo";
+    public const string MESSAGE_VALIDATION_ERRORS = "Errores de validación";
+    public const string MESSAGE_OPERATION_FAILED = "Operación fallida";
+    public const string MESSAGE_AUTH_TYPE = "Su cuenta no se encuentra registrada en el sistema";
+}
