086074cb1829d25a811fd9971a056b3fc11127be
CoreDX1 <chrismaquis@gmail.com>
2023-06-07 16:13:10 -0300

    Delete ID

--- src/File.Application/DTO/Request/Folder/FolderRequestDto.cs
+++ src/File.Application/DTO/Request/Folder/FolderRequestDto.cs
@@ -2,7 +2,6 @@ namespace File.Application.DTO.Request.Folder;
 
 public class FolderRequestDto
 {
-    public int Id { get; set; }
     public string? Name { get; set; }
     public string? Path { get; set; }
     public short Size { get; set; }
