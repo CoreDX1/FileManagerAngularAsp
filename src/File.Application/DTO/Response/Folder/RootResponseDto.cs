042aadca8bcebef5319425c46bd6453cd51844e3
CoreDX1 <chrismaquis@gmail.com>
2023-06-08 18:51:00 -0300

    Add Molds

--- src/File.Application/DTO/Response/Folder/RootResponseDto.cs
+++ src/File.Application/DTO/Response/Folder/RootResponseDto.cs
@@ -4,7 +4,18 @@ namespace File.Application.DTO.Response.Folder;
 
 public class RootResponseDto
 {
+    public string? Path { get; set; }
     public bool IsSuccess { get; set; }
     public IEnumerable<FolderResponseDto>? Directories { get; set; }
-    public List<FileResponseDto>? Files { get; set; }
+    public IEnumerable<FileResponseDto>? Files { get; set; }
+
+    // TODO : Additional information about the root directory
+    public long TotalSize { get; set; }
+    public DateTime LastModified { get; set; }
+    public string? Author { get; set; }
+
+    // TODO : Stadistic about the files and directories
+    public int FileCount => Files?.Count() ?? 0;
+    public int DirectoryCount => Directories?.Count() ?? 0;
+    public long TotalSizeInBytes { get; set; }
 }
