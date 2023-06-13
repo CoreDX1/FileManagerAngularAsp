040afe65146d77666ae0c6b6f93b2c091986a831
CoreDX1 <chrismaquis@gmail.com>
2023-06-13 14:48:44 -0300

    Created new class

---
+++ src/File.Application/Services/UtilsApplication.cs
@@ -0,0 +1,71 @@
+using System.Web;
+using File.Domain.Entities;
+using File.Infrastructure.Persistences.Interfaces;
+
+namespace File.Application.Services;
+
+public class UtilsApplication
+{
+    private readonly string baseDirectory;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UtilsApplication(IUnitOfWork unitOfWork)
+    {
+        baseDirectory = @"C:\Users\Christian\Desktop\";
+        _unitOfWork = unitOfWork;
+    }
+
+    public string DirectoryExists(string path)
+    {
+        string decodedPath = HttpUtility.UrlDecode(path);
+        string directoryPath = Path.Combine(baseDirectory, decodedPath.Replace('/', '\\'));
+        if (!Directory.Exists(directoryPath))
+            return null!;
+        return directoryPath;
+    }
+
+    public long AllDirectorySize(string[] dir, string[] fil)
+    {
+        long size = 0;
+        foreach (var fileInfo in dir)
+        {
+            var directoryInfo = new DirectoryInfo(fileInfo);
+            size += GetDirectorySize(directoryInfo);
+        }
+        foreach (var file in fil)
+        {
+            var fileInfo = new FileInfo(file);
+            size += fileInfo.Length;
+        }
+        return size;
+    }
+
+    // TODO : Method that used recursively
+    public long GetDirectorySize(DirectoryInfo directoryInfo)
+    {
+        long size = 0;
+        var files = directoryInfo.GetFiles();
+        foreach (var file in files)
+        {
+            size += file.Length;
+        }
+        var subdirectories = directoryInfo.GetDirectories();
+        foreach (var subdirectory in subdirectories)
+        {
+            size += GetDirectorySize(subdirectory);
+        }
+        return size;
+    }
+
+    public Folder ViewFolder(string path, string name)
+    {
+        var folderInfo = _unitOfWork.FolderRepository.GetByPath(path, name);
+        return new Folder()
+        {
+            Name = folderInfo.Name,
+            UserId = folderInfo.UserId,
+            Path = folderInfo.Path,
+            CreateDate = folderInfo.CreateDate,
+        };
+    }
+}
