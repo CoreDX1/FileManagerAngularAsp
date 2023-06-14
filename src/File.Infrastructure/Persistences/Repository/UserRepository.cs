eb787d5abff05d99a40f745f491734d13cd3aa68
CoreDX1 <chrismaquis@gmail.com>
2023-06-14 14:50:40 -0300

    Creating of a new method

--- src/File.Infrastructure/Persistences/Repository/UserRepository.cs
+++ src/File.Infrastructure/Persistences/Repository/UserRepository.cs
@@ -1,6 +1,7 @@
 using File.Domain.Entities;
 using File.Infrastructure.Persistences.Context;
 using File.Infrastructure.Persistences.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace File.Infrastructure.Persistences.Repository;
 
@@ -18,5 +19,11 @@ public class UserRepository : IUserRepository
         await this.context.AddAsync(user);
         int result = await this.context.SaveChangesAsync();
         return result > 0;
+    }
+
+    public async Task<User> GetUserByEmail(string email)
+    {
+        User? user = await this.context.Users.FirstOrDefaultAsync(x => x.Email == email);
+        return user!;
     }
 }
