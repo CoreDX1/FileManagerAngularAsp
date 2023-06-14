835b4243993006c3c733974c1f28bb91342fd9ce
CoreDX1 <chrismaquis@gmail.com>
2023-06-14 14:50:56 -0300

    Creating of a new Controller

---
+++ src/File.Api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+using File.Application.DTO.Request.User;
+using File.Application.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace File.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUserApplication app;
+
+        public UserController(IUserApplication app)
+        {
+            this.app = app;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> CreateUser([FromBody] UserRegisterRequestDto user)
+        {
+            var response = await this.app.AddUser(user);
+            return Ok(response);
+        }
+    }
+}
