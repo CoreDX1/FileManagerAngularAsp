d2ab515a0b029caa4db27de6ef4b00b0b9b5179b
CoreDX1 <chrismaquis@gmail.com>
2023-06-14 14:54:06 -0300

    Creating a class for the User Application

---
+++ src/File.Application/Services/UserApplication.cs
@@ -0,0 +1,67 @@
+using AutoMapper;
+using File.Application.Commons.Base;
+using File.Application.DTO.Request.User;
+using File.Application.DTO.Response.User;
+using File.Application.Interface;
+using File.Domain.Entities;
+using File.Infrastructure.Persistences.Interfaces;
+using File.Utilities.Static;
+
+namespace File.Application.Services;
+
+public class UserApplication : IUserApplication
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserApplication(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<BaseResponse<UserRegisterResponseDto>> AddUser(UserRegisterRequestDto user)
+    {
+        var response = new BaseResponse<UserRegisterResponseDto>();
+        var validUser = await GetUserByEmail(user.Email);
+        if (!validUser.Success)
+        {
+            response.Success = false;
+            response.Message = ReplyMessage.MESSAGE_OPERATION_FAILED;
+            return response;
+        }
+
+        User userEntity = this._mapper.Map<User>(user);
+        bool result = await this._userRepository.AddUser(userEntity);
+        if (!result)
+        {
+            response.Success = false;
+            response.Message = ReplyMessage.MESSAGE_SAVE_ERROR;
+        }
+        else
+        {
+            response.Success = true;
+            response.Message = ReplyMessage.MESSAGE_SAVE_SUCCESS;
+            response.Data = this._mapper.Map<UserRegisterResponseDto>(userEntity);
+        }
+        return response;
+    }
+
+    public async Task<BaseResponse<UserRegisterResponseDto>> GetUserByEmail(string email)
+    {
+        var response = new BaseResponse<UserRegisterResponseDto>();
+        User? user = await this._userRepository.GetUserByEmail(email);
+        if (user != null)
+        {
+            response.Success = false;
+            response.Message = ReplyMessage.MESSAGE_QUERY_EMPTY;
+        }
+        else
+        {
+            response.Success = true;
+            response.Message = ReplyMessage.MESSAGE_QUERY_SUCCESS;
+            response.Data = this._mapper.Map<UserRegisterResponseDto>(user);
+        }
+        return response;
+    }
+}
