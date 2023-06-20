29330d382cb690e88852f2779890be0b1fa9c9f6
CoreDX1 <chrismaquis@gmail.com>
2023-06-20 20:09:56 -0300

    A New Method for UserController

--- src/File.Api/Controllers/UserController.cs
+++ src/File.Api/Controllers/UserController.cs
@@ -22,5 +22,13 @@ namespace File.Api.Controllers
             var response = await this.app.AddUser(user);
             return Ok(response);
         }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginUser([FromBody] UserLoginRequestDto user)
+        {
+            var response = await this.app.LoginUser(user);
+            return Ok(response);
+        }
     }
 }
